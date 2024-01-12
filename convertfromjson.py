59b4147cd43c7c61d8b63bf507755d59595b5d85
papa <actechforlife@gmail.com>
2024-01-13 00:00:46 +0800

    converter

---
+++ convertfromjson.py
@@ -0,0 +1,42 @@
+import pdfkit
+from jinja2 import Environment, FileSystemLoader
+import os
+import json
+
+# Specify the path to wkhtmltopdf executable
+config = pdfkit.configuration(wkhtmltopdf='/usr/local/bin/wkhtmltopdf')  # Replace with your actual path
+
+# Load JSON data from the file
+resume_path = 'assets/json/resume.json'  # Update this path to the actual location of your resume.json file
+with open(resume_path, 'r') as json_file:
+    resume_data = json.load(json_file)
+
+# Load Jinja2 template
+env = Environment(loader=FileSystemLoader('.'))
+template = env.get_template('resume_template.html')
+
+# Render HTML using the template
+html_content = template.render(resume=resume_data)
+
+# Save HTML content to a file
+html_file_path = 'resume_generated.html'
+with open(html_file_path, 'w') as html_file:
+    html_file.write(html_content)
+
+# Specify CSS styling for the PDF
+css_path = 'assets/css/resume.css'  # Update this path to the actual location of your resume.css file
+with open(css_path, 'r') as css_file:
+    css_style = css_file.read()
+  
+
+# Save CSS styling to a file
+css_file_path = 'resume_style.css'
+with open(css_file_path, 'w') as css_file:
+    css_file.write(css_style)
+
+# Convert HTML to PDF
+pdfkit.from_file(html_file_path, 'resume_generated.pdf', configuration=config, css=css_file_path)
+
+# Clean up temporary HTML file and CSS file
+os.remove(html_file_path)
+os.remove(css_file_path)
