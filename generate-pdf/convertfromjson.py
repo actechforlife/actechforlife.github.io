0130ae8ba59886416672d30c15e62d39cca9fcc0
Acquah Hackman <actechforlife@gmail.com>
2024-01-15 11:05:11 +0800

    restructure

--- generate-pdf/convertfromjson.py
+++ generate-pdf/convertfromjson.py
@@ -4,7 +4,9 @@ import os
 import json
 
 # Specify the path to wkhtmltopdf executable
-config = pdfkit.configuration(wkhtmltopdf='/usr/local/bin/wkhtmltopdf')  # Replace with your actual path
+wk_path_ubuntu = '/usr/bin/wkhtmltopdf'
+wk_path_mac = '/usr/local/bin/wkhtmltopdf'
+config = pdfkit.configuration(wkhtmltopdf=wk_path_ubuntu)  # Replace with your actual path
 
 # Load JSON data from the file
 resume_path = 'assets/json/resume.json'  # Update this path to the actual location of your resume.json file
@@ -13,13 +15,13 @@ with open(resume_path, 'r') as json_file:
 
 # Load Jinja2 template
 env = Environment(loader=FileSystemLoader('.'))
-template = env.get_template('resume_template.html')
+template = env.get_template('generate-pdf/resume_template.html')
 
 # Render HTML using the template
 html_content = template.render(resume=resume_data)
 
 # Save HTML content to a file
-html_file_path = 'resume_generated.html'
+html_file_path = 'generate-pdf/resume_generated.html'
 with open(html_file_path, 'w') as html_file:
     html_file.write(html_content)
 
@@ -35,8 +37,15 @@ with open(css_file_path, 'w') as css_file:
     css_file.write(css_style)
 
 # Convert HTML to PDF
-pdfkit.from_file(html_file_path, 'resume_generated.pdf', configuration=config, css=css_file_path)
+try:
+    pdfkit.from_file(html_file_path, 'resume_generated.pdf', configuration=config, css=css_file_path)
+except:
+    # print('Error: wkhtmltopdf not found. Please install it.')
+    pass
+finally:
+    print('PDF generated successfully.')
+# pdfkit.from_file(html_file_path, 'resume_generated.pdf', configuration=config, css=css_file_path)
 
-# Clean up temporary HTML file and CSS file
-os.remove(html_file_path)
-os.remove(css_file_path)
+    # Clean up temporary HTML file and CSS file
+    os.remove(html_file_path)
+    os.remove(css_file_path)
