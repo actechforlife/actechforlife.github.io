def0588d9fbdc142b2b499306f6b301e9c87278b
Acquah Hackman <actechforlife@gmail.com>
2024-02-08 10:53:11 +0800

    language translation

--- assets/js/resume.js
+++ assets/js/resume.js
@@ -1,17 +1,65 @@
+// Set up Vue-i18n
+Vue.use(VueI18n);
+// Get the default language of the user's browser
+const defaultLanguage = navigator.language || navigator.userLanguage;
+
+// Extract the language code (e.g., 'en', 'fr', 'es')
+const languageCode = defaultLanguage.split('-')[0];
+console.log(languageCode);
+
+// Define supported locales
+const supportedLocales = ['en', 'zh']; // Add all supported locales
+
+// Set the default language if it's supported; otherwise, fallback to a default locale
+const defaultLocale = supportedLocales.includes(languageCode) ? languageCode : 'en';
+
+const i18n = new VueI18n({
+  locale: defaultLocale, // default locale
+  fallbackLocale: 'en', // fallback locale
+  messages: {
+    en: {
+      // English translations
+      //load greetings from resume.json {{}}
+      home: 'Home',
+      about: 'About',
+      skills: 'Skills',
+      resume: 'Resume',
+      projects: 'Projects',
+    },
+    zh: {
+      //chinese translations
+      home: '首页',
+      about: '关于',
+      skills: '技能',
+      resume: '简历',
+      projects: '项目',
+    }
+  },
+});
+
 new Vue({
-  el: '#main',
+  el: '#app',
   data: {
     resume: {}
   },
+  i18n,
   mounted() {
     this.loadResume();
-  },
-  computed: {
+  }, computed: {
     fullName() {
       return this.resume.Summary ? `${this.resume.Summary.fname} ${this.resume.Summary.lname}` : '';
     }
   },
   methods: {
+    switchLanguage(locale) {
+      this.$i18n.locale = locale;
+    },
+
+truncateDescription(description) {
+  const maxWords = 15;
+  const words = description.split(' ');
+  return words.length > maxWords ? `${words.slice(0, maxWords).join(' ')}...` : description;
+},
     async loadResume() {
       try {
         const response = await fetch('assets/json/resume.json');
@@ -23,12 +71,52 @@ new Vue({
         console.error('Error loading resume:', error);
       }
     },
-    truncateDescription(description) {
-      const maxWords = 15;
-      const words = description.split(' ');
-      return words.length > maxWords ? `${words.slice(0, maxWords).join(' ')}...` : description;
+    getName() {
+      if (this.resume && this.resume.Summary) {
+        switch (this.$i18n.locale) {
+          case 'en':
+            return this.fullName || '';
+          case 'zh':
+            return this.resume.Summary.chinesename || this.fullName || '';
+          default:
+            return this.fullName || '';
+        }
+      }
+      return '';
+    },
+
+    getDescription() {
+      // Return the description based on the current locale
+      if (this.resume && this.resume.Summary) {
+        switch (this.$i18n.locale) {
+          case 'en':
+            return this.resume.Summary.description;
+          case 'zh':
+            // Add support for Chinese description if available
+            return this.resume.Summary.chinesedescription || this.resume.Summary.description;
+          default:
+            return this.resume.Summary.description;
+        }
+      }
+      return '';
+    },
+    getAboutme() {
+      if (this.resume && this.resume.Summary) {
+        // Return the description based on the current locale                    
+        switch (this.$i18n.locale) {
+          case 'en':
+            return this.resume.Summary.aboutme;
+          case 'zh':
+            // Add support for Chinese description if available
+            return this.resume.Summary.chineseaboutme || this.resume.Summary.description;
+          default:
+            return this.resume.Summary.aboutme;
+        }
+      }
+      return '';
     },
     handleProjectDetails() {
+
       // Get the project index from the URL
       const urlParams = new URLSearchParams(window.location.search);
       const projectIndex = urlParams.get('project_index');
